54767a1ba05010f50aceb524013494bfce483cd3
KIM JI HYEON <kwlgus0318@gmail.com>
2022-06-14 14:13:32 +0900

    20220614

---
+++ index2.js
@@ -0,0 +1,10 @@
+const design = document.getElementsByClassName('design')
+console.log(design)
+
+const outdoor = document.getElementsByClassName('design-door')
+console.log(outdoor)
+
+
+outdoor,addEventListener('click', ()=>{
+  
+})
