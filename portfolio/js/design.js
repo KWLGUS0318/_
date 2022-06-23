df02a791e508bae4d3d3ee0d630d301e3d75c0a9
KIM JI HYEON <kwlgus0318@gmail.com>
2022-06-23 17:45:26 +0900

    20220623

--- portfolio/js/design.js
+++ portfolio/js/design.js
@@ -25,3 +25,43 @@ if (heightValue >= root.offsetHeight) {
 })
 
 //window.confirm('천천히 보고 싶으시다면 바탕을 눌러주세요~!')
+
+
+
+const slideContainer = document.getElementById('slide');
+const slideItems = Array.from(document.querySelectorAll("#slide li"));
+const left = document.getElementsById('left');
+const right = document.getElementsById('right');
+let current = 0;
+function displayControl(arr) {
+  arr.forEach((value) => {
+    let getDatasetValue = Number(value.dataset.slideItem);
+    if (getDatasetValue === current) {
+    value.style.display = "block";
+    } else {
+    value.style.display = "none";
+    }
+  });
+}
+displayControl(slideItems);
+function buttonEvent(eventTrigger, targetElement) {
+  eventTrigger.addEventListener('click', function () {
+    if (eventTrigger === left) {
+      current--;
+      if (current < 0) {
+        current = targetElement.length - 1;
+      }
+      displayControl(targetElement);
+    }
+    if (eventTrigger === right) {
+      current++;
+      if (current >= targetElement.length) {
+        current = 0;
+      }
+      displayControl(targetElement);
+    }
+  });
+};
+
+buttonEvent(left, slideItems);
+buttonEvent(right, slideItems);
