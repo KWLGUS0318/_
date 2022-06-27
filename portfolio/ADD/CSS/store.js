8ec47d32715f1cfc299f78ca188312cf237569fe
KIM JI HYEON <kwlgus0318@gmail.com>
2022-06-27 17:59:00 +0900

    20220627

--- portfolio/ADD/CSS/store.js
+++ portfolio/ADD/CSS/store.js
@@ -4,16 +4,32 @@ console.dir(next)
 const previous = document.querySelector('.previous')
 console.dir(previous)
 
+
+
 const food1 = document.querySelector('.food')
 const food2 = document.querySelector('.food-buy')
 console.log(food1)
 console.log(food2)
 
-next.addEventListener('click', function(){
-  if(food1.classList.contains('on')){
+
+next.addEventListener('click', function () {
+  if (food1.classList.add('on')) {} 
+  else {
+    food2.classList.add('off')
   }
-  else{food1.classList.add('on')}
+  food1.classList === 'on'
 })
 
+
+previous.addEventListener('click', function() {
+  if(food2.classList.contains('off')){
+
+  food1.classList.remove('on');
+  food2.classList.remove('off')
+  food2.classList.add('on');
+
+  }
+  
+})
 
 
