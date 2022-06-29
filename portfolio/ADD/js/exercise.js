cf2f73656f80d1ddccf2520eeb7d708f40194e20
KIM JI HYEON <kwlgus0318@gmail.com>
2022-06-29 12:52:05 +0900

    20220629

--- portfolio/ADD/js/exercise.js
+++ portfolio/ADD/js/exercise.js
@@ -1,12 +1,8 @@
-const foodSection = document.querySelector('.food-section')
-console.log(foodSection)
+const save = document.querySelector('.save')
+console.log(save)
+const box = document.querySelector('.box')
 
-const write = document.querySelector('.write')
-console.log(write)
-
-foodSection.addEventListener('click', function(){
-if(write.classList.contains('on')){
-  write.classList.remove('write')
-}
-else{write.classList.add('on')}
+save.addEventListener('click', function () {
+  window.confirm('저장할까요?')
 })
+
